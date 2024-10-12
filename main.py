ac33eab3ea5f55063f252b62fc0e34597c2974a7
Kenan Sezgin <kenansezgin@MacBook-Pro-von-Kenan.local>
2024-10-12 19:28:20 +0100

    Add venv database and mein file

---
+++ main.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+
+def create_db():
+
+    connection = sqlite3.connect("example_database.db")
+
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        age INTEGER,
+        email TEXT UNIQUE NOT NULL
+    )
+    """
+    )
+
+    cursor.execute(
+        """
+    INSERT INTO users (name, age, email) 
+    VALUES ('Raphael', 33, 'raphael@example.com')
+    """
+    )
+
+    connection.commit()
+    connection.close()
+
+    print("SQLite-Datenbank und Tabelle erfolgreich erstellt!")
+
+
+create_db()
+
+
