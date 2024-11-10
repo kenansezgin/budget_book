ee0c3d4ff715d0886a42dc58bb958dffb1303828
Kenan Sezgin <sezgin.k@outlook.de>
2024-11-10 22:44:32 +0000

    Tabellen umstrukturiert sodass die Monate in den Spalten verteilt sind

--- main.py
+++ main.py
@@ -1,61 +1,179 @@
-import sqlite3
+# Importiere benötigte Module
+import sqlite3  # Datenbank
+import glob     # Für Dateisuche im Dateisystem
+import os       # Für Dateisystemoperationen
+from datetime import datetime, date  # Für Datumsmanipulation
 
-# Erlaubt globale Variablen
-import glob
 
-# Erlaubt Dateisystemdaten
-import os
-from datetime import datetime, date
-
-# Konstante um den Datenbankpfad festzulegen
+# Definiere den Pfad zur SQLite-Datenbank
 DB_PATH = "./example.db"
 
-
+# Prüft, ob die Datenbank vorhanden ist und erstellt sie, falls nicht
 def check_db(conn):
-    # does the path exists
     if os.path.exists(DB_PATH):
-        # Connection of SQLite DB is now inside the variable conn
-
+        # Verbindet sich mit der SQLite-Datenbank
         create_table(conn)
         conn.close
 
-
+# Erstellt die Tabellen, wenn sie noch nicht existieren
 def create_table(conn):
     cursor = conn.cursor()
     cursor.execute(
         """
-    CREATE TABLE IF NOT EXISTS fix_costs (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        rent FLOAT,
-        electricity FLOAT,
-        gas FLOAT,
-        dsl FLOAT,
-        mobile_data FLOAT,
-        monthly_ticket FLOAT,
-        website_hosting FLOAT,
-        website FLOAT,
-        apple_icloud_50GB FLOAT
+        CREATE TABLE IF NOT EXISTS annual_overview (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            description TEXT NOT NULL,
+            budget NUMERIC,
+            january NUMERIC,
+            february NUMERIC,
+            march NUMERIC,
+            april NUMERIC,
+            may NUMERIC,
+            june NUMERIC,
+            july NUMERIC,
+            august NUMERIC,
+            september NUMERIC,
+            october NUMERIC,
+            november NUMERIC,
+            december NUMERIC,
+            total NUMERIC
         );
-    """
-    )
-    conn.commit()
 
-    cursor.execute(
+        -- Tabellen für alle Monate
+        CREATE TABLE IF NOT EXISTS january (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT,
+            food NUMERIC,
+            boldt NUMERIC,
+            groceries NUMERIC,
+            hygiene NUMERIC,
+            books NUMERIC,
+            museum NUMERIC
+        );
+        
+        CREATE TABLE IF NOT EXISTS february (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT,
+            food NUMERIC,
+            boldt NUMERIC,
+            groceries NUMERIC,
+            hygiene NUMERIC,
+            books NUMERIC,
+            museum NUMERIC
+        );
+        
+        CREATE TABLE IF NOT EXISTS march (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT,
+            food NUMERIC,
+            boldt NUMERIC,
+            groceries NUMERIC,
+            hygiene NUMERIC,
+            books NUMERIC,
+            museum NUMERIC
+        );
+        
+        CREATE TABLE IF NOT EXISTS april (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT,
+            food NUMERIC,
+            boldt NUMERIC,
+            groceries NUMERIC,
+            hygiene NUMERIC,
+            books NUMERIC,
+            museum NUMERIC
+        );
+        
+        CREATE TABLE IF NOT EXISTS may (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT,
+            food NUMERIC,
+            boldt NUMERIC,
+            groceries NUMERIC,
+            hygiene NUMERIC,
+            books NUMERIC,
+            museum NUMERIC
+        );
+        
+        CREATE TABLE IF NOT EXISTS june (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT,
+            food NUMERIC,
+            boldt NUMERIC,
+            groceries NUMERIC,
+            hygiene NUMERIC,
+            books NUMERIC,
+            museum NUMERIC
+        );
+        
+        CREATE TABLE IF NOT EXISTS july (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT,
+            food NUMERIC,
+            boldt NUMERIC,
+            groceries NUMERIC,
+            hygiene NUMERIC,
+            books NUMERIC,
+            museum NUMERIC
+        );
+        
+        CREATE TABLE IF NOT EXISTS august (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT,
+            food NUMERIC,
+            boldt NUMERIC,
+            groceries NUMERIC,
+            hygiene NUMERIC,
+            books NUMERIC,
+            museum NUMERIC
+        );
+        
+        CREATE TABLE IF NOT EXISTS september (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT,
+            food NUMERIC,
+            boldt NUMERIC,
+            groceries NUMERIC,
+            hygiene NUMERIC,
+            books NUMERIC,
+            museum NUMERIC
+        );
+        
+        CREATE TABLE IF NOT EXISTS october (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT,
+            food NUMERIC,
+            boldt NUMERIC,
+            groceries NUMERIC,
+            hygiene NUMERIC,
+            books NUMERIC,
+            museum NUMERIC
+        );
+        
+        CREATE TABLE IF NOT EXISTS november (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT,
+            food NUMERIC,
+            boldt NUMERIC,
+            groceries NUMERIC,
+            hygiene NUMERIC,
+            books NUMERIC,
+            museum NUMERIC
+        );
+        
+        CREATE TABLE IF NOT EXISTS december (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT,
+            food NUMERIC,
+            boldt NUMERIC,
+            groceries NUMERIC,
+            hygiene NUMERIC,
+            books NUMERIC,
+            museum NUMERIC
+        );
         """
-    CREATE TABLE IF NOT EXISTS expenses (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        date DATE NOT NULL,
-        food FLOAT,
-        groceries FLOAT,
-        boldt FLOAT,
-        hygiene FLOAT,
-        books FLOAT,
-        museum FLOAT
-        );
-    """
     )
-    conn.commit()
-
+    conn.commit()  # Speichert Änderungen in der Datenbank
 
 # Adapter: datetime.date -> str (für Speicherung im deutschen Format TT.MM.JJJJ)
 sqlite3.register_adapter(date, lambda d: d.strftime("%d.%m.%Y"))
@@ -66,58 +184,87 @@ sqlite3.register_converter(
     "DATE", lambda s: datetime.strptime(s.decode(), "%d.%m.%Y").date()
 )
 
+# Holt das heutige Datum im Format TT.MM.JJJJ
+today_date = datetime.now().date()
 
-# Aktuelles Datum abrufen
-today_date = datetime.now().date()  # Heute als Datum (TT.MM.JJJJ)
-
-
-def add_fix_costs(conn):
+# Fügt festgelegte Fixkosten in die Tabelle ein
+def add_fix_values(conn):
     cursor = conn.cursor()
     cursor.execute(
         """
-        INSERT INTO fix_costs (rent, electricity, gas, dsl, mobile_data, monthly_ticket, website_hosting, website, apple_icloud_50GB)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        INSERT INTO annual_overview (description, budget, january, february, march, april, may, june, july, august, september, november, december, total)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         """,
-        (0.0, 0.0, 0.0, 0.0, 40.0, 1.90, 0.0, 1.0, 0.0),
+        [
+            ("income", 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000),
+            ("rent", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            ("electricity", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            ("gas", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            ("dsl", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            ("mobile_data", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            ("monthly_ticket", 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40),
+            ("website_hosting", 1.90, 1.90, 1.90, 1.90, 1.90, 1.90, 1.90, 1.90, 1.90, 1.90, 1.90, 1.90, 1.90),
+            ("website", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            ("apple_icloud_50GB", 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+        ],
     )
+    conn.commit()  # Speichert Änderungen in der Datenbank
 
-
+# Sammelt Benutzereingaben für tägliche Ausgaben und speichert diese
 def collect_expenses(conn):
+    cursor = conn.cursor()
 
-    # Benutzereingaben für Ausgaben
-    food_value = float(input("Heutige Ausgaben für Essen?: "))
-    groceries_value = float(input("Heutige Ausgaben für Lebensmittel?: "))
-    boldt_value = float(input("Heutige Ausgaben für Boldt?: "))
+    # Benutzereingaben für verschiedene Ausgabenkategorien
+    while True:
+        try:
+            food_value = float(input("Heutige Ausgaben für Essen?: "))
+            break
+        except ValueError:
+            print("Das war keine korrekte Angabe. Bitte versuche es erneut.")
 
-    # Einfügen der Daten für das heutige Datum
+    while True:
+        try:
+            groceries_value = float(input("Heutige Ausgaben für Lebensmittel?: "))
+            break
+        except ValueError:
+            print("Das war keine korrekte Angabe. Bitte versuche es erneut.")
+
+    while True:
+        try:
+            boldt_value = float(input("Heutige Ausgaben für Boldt?: "))
+            break
+        except ValueError:
+            print("Das war keine korrekte Angabe. Bitte versuche es erneut.")
+
+    # Einfügen der heutigen Ausgaben in die Tabelle
+    # Programm muss erkennen welches Datum wir heute haben und es in die richtige Tabelle mit dem richtigen Monat in die Zeile mit dem richtigen Monat hineinsetzen.
+    # das benötige ich print(  today_date.month) und ich benötige ich vermutlich eine dictionary
     cursor.execute(
         """
-        INSERT INTO expenses (date, food, groceries, boldt)
-        VALUES (?, ?, ?, ?)
+        INSERT INTO variable_costs (date, description, budget, january, february, march, april, may, june, july, august, september, october, november, december)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         """,
-        (today_date, food_value, groceries_value, boldt_value),
-    )
+        [today_date, "food", food_value, groceries_value, boldt_value]
 
+        
+      
+    )
+    conn.commit()
 
 if __name__ == "__main__":
-
+    # Verbindung zur SQLite-Datenbank
     conn = sqlite3.connect(DB_PATH)
-    create_table(conn)
-""""
+
+    # Überprüfen und Erstellen der Datenbank und Tabellen
     check_db(conn)
-  
-    # Fix kosten sammeln
-    add_fix_costs(conn)
 
-    # Ausgaben sammeln
-    collect_expenses()
+    # Festgelegte Fixkosten sammeln und in die Datenbank einfügen
+    add_fix_values(conn)
 
-    # Änderungen speichern und Verbindung schließen
-    # connection.commit()
-    conn.close(conn)
+    # Tägliche Ausgaben sammeln und in die Datenbank einfügen
+    collect_expenses(conn)
 
-    print("SQLite-Datenbank und Tabelle erfolgreich erstellt und Daten hinzugefügt!")
+    # Verbindung zur Datenbank schließen
+    conn.close()
 
-
-# library streamlit: um eine webseite zu erstellen und datenbanken zu füllen
-"""
+    print("SQLite-Datenbank und Tabellen erfolgreich erstellt und Daten hinzugefügt!")
