1e616438411d6f878f7be6f028d3787d982b64d8
Kenan Sezgin <sezgin.k@outlook.de>
2025-01-19 21:19:15 +0000

    Zeile 43: Versuche Updatebefehl um die Summe von allen im Januar verkauften Prodkten als Ergebnis zu bekommen

--- alternative_1.py
+++ alternative_1.py
@@ -1,3 +1,5 @@
+# Zeile 19 und 23 überlege dir neue Spalten und entscheide was die Spalten sein sollen und was die Zeilen sein sollen
+
 import sqlite3
 from datetime import datetime
 
@@ -6,17 +8,17 @@ def nutzereingabe():
     produkt_name = input(f"Gebe den Produktnamen ein: ")
     menge = int(input(f"Gebe die Menge ein: "))
     preis = float(input(f"Gebe die Preis ein: "))
-    datum = str(input(f"Gebe Einkaufsmonat ein oder drücke Enter für heutiges Datum: "))
-    if not datum:
-        datum = datetime.now().strftime("%B")
-    return produkt_name, menge, preis, datum
+    monat = str(input(f"Gebe Einkaufsmonat ein oder drücke Enter für heutiges monat: "))
+    if not monat:
+        monat = datetime.now().strftime("%B")
+    return produkt_name, menge, preis, monat
 
 
 def create_table():
     connection = sqlite3.connect("alternative_1.db")
     cursor = connection.cursor()
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS verkaufstabelle (id INTEGER PRIMARY KEY, produkt_name TEXT, menge INTEGER, preis REAL, umsatz REAL)"
+        "CREATE TABLE IF NOT EXISTS verkaufstabelle (id INTEGER PRIMARY KEY, produkt_name TEXT, menge INTEGER, preis REAL, monat TEXT, umsatz REAL)"
     )
 
     cursor.execute(
@@ -27,15 +29,18 @@ def create_table():
 
 
 def insert_values(df):
-    produkt_name, menge, preis, datum = df
+    produkt_name, menge, preis, monat = df
     connection = sqlite3.connect("alternative_1.db")
     cursor = connection.cursor()
     cursor.execute(
-        "INSERT OR IGNORE INTO verkaufstabelle (produkt_name, menge, preis, umsatz) VALUES (?, ?, ?, ?);",
-        (produkt_name, menge, preis, datum),
+        "INSERT OR IGNORE INTO verkaufstabelle (produkt_name, menge, preis, umsatz) VALUES (?, ?, ?, ?, ?);",
+        (produkt_name, menge, preis, monat, menge * preis),
     )
 
-    cursor.execute("INSERT INTO monatsübersicht (monat) VALUES ('januar');")
+    cursor.execute(
+        "INSERT INTO monatsübersicht (monat) VALUES ('?');",
+        (monat),
+    )
 
     # Änderungen speichern und Verbindung schließen
     connection.commit()
--- alternative_2.py
+++ alternative_2.py
@@ -6,16 +6,16 @@ def nutzereingabe():
     produkt_name = input(f"Gebe den Produktnamen ein: ")
     menge = int(input(f"Gebe die Menge ein: "))
     preis = float(input(f"Gebe die Preis ein: "))
-    datum = str(input(f"Gebe Einkaufsmonat ein oder drücke Enter für heutiges Datum: "))
-    if not datum:
-        datum = datetime.now().strftime("%B")
-    return produkt_name, menge, preis, datum
+    monat = str(input(f"Gebe Einkaufsmonat ein oder drücke Enter für heutiges monat: "))
+    if not monat:
+        monat = datetime.now().strftime("%B")
+    return produkt_name, menge, preis, monat
 
 
 def create_table(connection, cursor):
 
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS verkaufstabelle (id INTEGER PRIMARY KEY, produkt_name TEXT, menge INTEGER, preis REAL, umsatz REAL)"
+        "CREATE TABLE IF NOT EXISTS verkaufstabelle (id INTEGER PRIMARY KEY, produkt_name TEXT, menge INTEGER, preis REAL, monat TEXT, umsatz REAL)"
     )
 
     cursor.execute(
@@ -24,15 +24,25 @@ def create_table(connection, cursor):
     connection.commit()
 
 
+
+
 def insert_values(connection, cursor, df):
-    produkt_name, menge, preis, datum = df
+    produkt_name, menge, preis, monat = df
 
     cursor.execute(
-        "INSERT OR IGNORE INTO verkaufstabelle (produkt_name, menge, preis, umsatz) VALUES (?, ?, ?, ?);",
-        (produkt_name, menge, preis, datum),
+        "INSERT OR IGNORE INTO verkaufstabelle (produkt_name, menge, preis, umsatz) VALUES (?, ?, ?, ?, ?);",
+        (produkt_name, menge, preis, monat, menge * preis),
     )
 
-    cursor.execute("INSERT INTO monatsübersicht (monat) VALUES ('januar');")
+    cursor.execute(
+        "INSERT INTO monatsübersicht (monat) VALUES ('?');",
+        (monat),
+    )
+
+
+    cursor.execute(
+        "UPDATE monatsübersicht SET gesamtumsatz = (SELECT SUM(umsatz) FROM verkaufstabelle) WHERE monat = 'January';
+    )
 
     # Änderungen speichern und Verbindung schließen
     connection.commit()
@@ -53,3 +63,16 @@ if __name__ == "__main__":
 
 # Gibt aus ob Änderungen stattgefunden haben
 # print(connection.total_changes)
+
+
+
+# UPDATE table_name
+# SET column1 = value1, column2 = value2, ...
+# WHERE condition;
+
+# SELECT column1, column2, ...
+# FROM table_name;
+
+# SELECT SUM(column_name)
+# FROM table_name
+# WHERE condition; 
