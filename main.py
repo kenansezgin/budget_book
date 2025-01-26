076219787340d94edaa2a30fbae5b7cf714ef11f
Kenan Sezgin <sezgin.k@outlook.de>
2025-01-26 21:33:35 +0000

    Create View verwendet für Monatsübersicht

--- main.py
+++ main.py
@@ -3,69 +3,85 @@ from datetime import datetime
 
 
 def nutzereingabe():
-    produkt_name = input(f"Gebe den Produktnamen ein: ")
-    menge = int(input(f"Gebe die Menge ein: "))
-    preis = float(input(f"Gebe die Preis ein: "))
-    datum = str(input(f"Gebe Einkaufsmonat ein oder drücke Enter für heutiges Datum: "))
-    if not datum:
-        datum = datetime.now().strftime("%B")
-    return produkt_name, menge, preis, datum
-
-
-def create_db():
-    # Gibt uns ein Objekt "connection" zurück damit wir mit der Datenbank interaggieren können
-    connection = sqlite3.connect("products.db")
-
-    # Erlaubt uns SQL Befehle an die SQL Datenbank zu senden
-    cursor = connection.cursor()
-    return connection, cursor
+    produkt_name = input("Gebe den Produktnamen ein: ")
+    menge = int(input("Gebe die Menge ein: "))
+    preis = float(input("Gebe die Preis ein: "))
+    monat = input("Gebe Einkaufsmonat ein oder drücke Enter für heutiges monat: ")
+    if not monat:
+        monat = datetime.now().strftime("%B")
+    return produkt_name, menge, preis, monat
 
 
 def create_table(connection, cursor):
-    cursor.execute(
-        "CREATE TABLE IF NOT EXISTS verkaufstabelle (id INTEGER PRIMARY KEY, produkt_name TEXT, menge INTEGER, preis REAL, umsatz REAL)"
-    )
 
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS monatsübersicht (monat TEXT, gesamtumsatz REAL, gesamt_verkaufsmenge INTEGER)"
+        "CREATE TABLE IF NOT EXISTS verkaufstabelle (id INTEGER PRIMARY KEY, produkt_name TEXT, menge INTEGER, preis REAL, umsatz REAL, monat TEXT)"
     )
+
+    connection.commit()
 
 
 def insert_values(connection, cursor, df):
+    produkt_name, menge, preis, monat = df
 
     cursor.execute(
-        "INSERT OR IGNORE INTO verkaufstabelle (produkt_name, menge, preis, umsatz) VALUES ('shorts', 12, 9.99, 6.99);"
+        "INSERT OR IGNORE INTO verkaufstabelle (produkt_name, menge, preis, umsatz, monat) VALUES (?, ?, ?, ?, ?);",
+        (produkt_name, menge, preis, menge * preis, monat),
     )
 
-    cursor.execute("INSERT INTO monatsübersicht (monat) VALUES ('januar');")
+    cursor.execute(
+        """
+        CREATE VIEW IF NOT EXISTS monatsübersicht AS
+        SELECT
+            produkt_name,
+            SUM(menge) AS verkaufsmenge_gesamt,
+            SUM (umsatz) AS umsatz_gesamt,
+            monat
+        FROM verkaufstabelle
+        GROUP BY produkt_name, monat;
+        """
+    )
 
     # Änderungen speichern und Verbindung schließen
     connection.commit()
-    connection.close()
 
 
 def main():
+    connection = sqlite3.connect("products.db")
+    cursor = connection.cursor()
+
     df = nutzereingabe()
-    x = create_db()
-    create_table(x[0], x[1])
-    insert_values(x[0], x[1], df)
+    create_table(connection, cursor)
+    insert_values(connection, cursor, df)
+    connection.close()
 
 
 if __name__ == "__main__":
     main()
 
 
-"""
-# Name der SQLite-Datenbank
-DATABASE_NAME = 'expenses.db'
+#  cursor.execute(
+#         "INSERT OR IGNORE INTO monatsübersicht (produkt_name, verkaufsmenge_gesamt, umsatz_gesamt, monat) VALUES (?, ?, ?, ?);",
+#         (produkt_name, monat,),
+#     )
 
-# Verbindung zur SQLite-Datenbank herstellen
-connection = sqlite3.connect(DATABASE_NAME)
-cursor = connection.cursor()
-
-    # Tabellen erstellen
-create_tables(cursor)
-"""
 
 # Gibt aus ob Änderungen stattgefunden haben
 # print(connection.total_changes)
+
+
+# REATE VIEW view_name AS
+# SELECT column1, column2, ...
+# FROM table_name
+# WHERE condition;
+
+# UPDATE table_name
+# SET column1 = value1, column2 = value2, ...
+# WHERE condition;
+
+# SELECT column1, column2, ...
+# FROM table_name;
+
+# SELECT SUM(column_name)
+# FROM table_name
+# WHERE condition;
